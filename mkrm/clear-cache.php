3cb25be758aaaab647bf0221d7d519de244f6bf2
medicamallit-it <medicamallit@gmail.com>
2026-06-08 15:00:12 +0300

    Delete mkrm directory

--- mkrm/clear-cache.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('X-LiteSpeed-Purge: *');
-header('Cache-Control: no-cache, no-store, must-revalidate');
-header('Pragma: no-cache');
-header('Expires: 0');
-echo 'Cache purged at ' . date('Y-m-d H:i:s');
-?>
